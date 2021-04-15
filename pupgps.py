671b4be710d4c0b12d2e6ec23e92a5ce5f9851a3
Colin Wu <colinwu1418@gmail.com>
2021-04-15 10:13:04 -0400

    update comments

--- pupgps.py
+++ pupgps.py
@@ -14,7 +14,7 @@ PUP-2, 2021 ABE Capstone
 
 This program receives data from GPS and GPSD modules and returns the location info, such as latitude and longitude.
 
-Return format: date, time, latitude, longitude, altitude, speed
+Return format: date, time, latitude, longitude, altitude, speed, number of satellites, vehicle ID
 
 To set up the GPSD module, watch this video: https://www.youtube.com/watch?v=isVHkovZuSM
 Reference of code: https://ozzmaker.com/using-python-with-a-gps-receiver-on-a-raspberry-pi/
@@ -24,6 +24,7 @@ Reference of code: https://ozzmaker.com/using-python-with-a-gps-receiver-on-a-ra
 def Navidata(vid):
     gpsd = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
 
+    data = ""
     while True:
         report = gpsd.next()  #
         if report['class'] == 'TPV':
@@ -36,9 +37,9 @@ def Navidata(vid):
             data = str(lat) + " " + str(lon) + " " + str(alt) + " " + str(speed)
             
         if report['class'] == 'SKY':
+            # Retrieve the number of satellites
             data += " " + str(len(gpsd.satellites)) + " " + vid
-        
-            # Print output; format: date | time | latitude | longitude | altitude | speed | vehicle ID
+
             os.system("echo " + str(
                 datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + " " + data + " >> ./data/gps_data.txt")
 
