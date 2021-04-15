196f94df406d3ee5642c493b0bf2a28e55153b63
Colin Wu <colinwu1418@gmail.com>
2021-04-15 11:16:45 -0400

    add fix quality

--- pupgps.py
+++ pupgps.py
@@ -14,7 +14,7 @@ PUP-2, 2021 ABE Capstone
 
 This program receives data from GPS and GPSD modules and returns the location info, such as latitude and longitude.
 
-Return format: date, time, latitude, longitude, altitude, speed, number of satellites, vehicle ID
+Return format: date, time, latitude, longitude, altitude, speed, fix quality, number of satellites, vehicle ID
 
 To set up the GPSD module, watch this video: https://www.youtube.com/watch?v=isVHkovZuSM
 Reference of code: https://ozzmaker.com/using-python-with-a-gps-receiver-on-a-raspberry-pi/
@@ -28,18 +28,19 @@ def Navidata(vid):
     while True:
         report = gpsd.next()  #
         if report['class'] == 'TPV':
-            # Retrieve latitude, longitude, altitude, and speed
+            # Retrieve latitude, longitude, altitude, speed, and fix quality
             lat = getattr(report, 'lat', 0.0)
             lon = getattr(report, 'lon', 0.0)
             alt = getattr(report, 'alt', 'nan')
             speed = getattr(report, 'speed', 'nan')
+            status = getattr(report, 'status', 'nan')
 
-            data = str(lat) + " " + str(lon) + " " + str(alt) + " " + str(speed)
+            data = str(lat) + " " + str(lon) + " " + str(alt) + " " + str(speed) + " " + str(status)
             
         if report['class'] == 'SKY':
             # Retrieve the number of satellites
             data += " " + str(len(gpsd.satellites)) + " " + vid
-
+            
             os.system("echo " + str(
                 datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + " " + data + " >> ./data/gps_data.txt")
 
