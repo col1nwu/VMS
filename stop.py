9a9ae11312f06a1cf09c58ad7e8bef38d4d5a5b0
Colin Wu <colinwu1418@gmail.com>
2021-04-21 21:03:13 -0400

    update comments

--- stop.py
+++ stop.py
@@ -6,24 +6,31 @@ GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 
+
 def stop():
+    # Open pid files and read pid
     file = open("./data/pid.txt", "r")
 
     pid1 = file.readline().strip()
     pid2 = file.readline().strip()
     pid3 = file.readline().strip()
 
+    file.close()
+
+    # For testing purpose
     print(pid1)
     print(pid2)
     print(pid3)
 
+    # Kill three Python processes
     os.system("kill " + pid1)
     os.system("kill " + pid2)
     os.system("kill " + pid3)
 
-    # Shut down the LED's too
+    # Turn down the LED's in case they are on when the system is shut down
     GPIO.output(23, GPIO.LOW)
     GPIO.output(18, GPIO.LOW)
+
 
 if __name__ == "__main__":
     stop()
